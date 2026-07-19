ee582ff3eec8bd68dd2660c9109ca4e8dd042201
sufyansaeed1343 <shaikhhassnain628@gmail.com>
2026-07-19 11:38:38 +0500

    app.py

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file, jsonify
+import requests
 import os
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
+HF_TOKEN = os.environ.get("HF_TOKEN")
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+
+headers = {
+    "Authorization": f"Bearer {HF_TOKEN}"
+}
+
+
+@app.route("/")
 def home():
     return "SUFI VISION AI API is running"
 
-@app.route('/generate', methods=['POST'])
+
+@app.route("/generate", methods=["POST"])
 def generate():
-    data = request.json
-    prompt = data.get('prompt', 'test')
+    data = request.get_json()
 
-    return jsonify({
-        "status": "success",
-        "prompt": prompt,
-        "message": "API working on Railway"
-    })
+    prompt = data.get("prompt", "A futuristic city")
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    response = requests.post(
+        API_URL,
+        headers=headers,
+        json={"inputs": prompt},
+        timeout=300
+    )
+
+    if response.status_code != 200:
+        return jsonify({
+            "error": response.text
+        }), 500
+
+    return send_file(
+        io.BytesIO(response.content),
+        mimetype="image/png"
+    )
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
